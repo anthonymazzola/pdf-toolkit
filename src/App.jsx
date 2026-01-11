c4414a45545078a6739184f4f9eb626705e15091
Anthony Mazzola <apmazzol@uvm.edu>
2026-01-11 15:16:37 -0500

    Fix footer positioning on initial load

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function NavLink({ to, children }) {
 
 function Layout() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="flex-1 bg-gray-50 flex flex-col">
       {/* Header - Full width */}
       <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
         <div className="w-full px-4 sm:px-6 lg:px-12 h-14 sm:h-16 flex items-center justify-between">
